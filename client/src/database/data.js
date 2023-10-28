fbe7f96b4458c9519ad9ecdf12b81532404c2f27
Cesar <cesar_sevilla@outlook.com>
2023-10-27 18:11:42 -0600

    Add data: example data for app

---
+++ client/src/database/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id: 1,
+        question: "Question 1",
+        options: [
+            'option 1',
+            'option 2',
+            'option 3',
+        ]
+    },
+    {
+        id: 2,
+        question: "Question 2",
+        options: [
+            'option 1',
+            'option 2',
+            'option 3',
+        ]
+    },
+    {
+        id: 3,
+        question: "Question 3",
+        options: [
+            'option 1',
+            'option 2',
+            'option 3',
+        ]
+    }
+]
